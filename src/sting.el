0bc7e03eb5d3a68ba5fa3e9f59c81d6d6854fea9
Leo Valais <leo.valais97@gmail.com>
2019-08-01 00:58:14 +0200

    wip:emacs: add C-c C-c front-end mechanism

--- src/sting.el
+++ src/sting.el
@@ -60,16 +60,17 @@
                                 (sting-pass-report 'sting-success-indicator-face)
                                 (sting-fail-report (ecase (sting-fail-report-kind report)
                                                      (:assertion 'sting-failure-indicator-face)
-                                                     (:timeout 'sting-timeout-indicator-face))))))
-    (insert " ")
+                                                     (:timeout 'sting-timeout-indicator-face))))
+                        'sting-test test))
+    (insert (propertize " " 'sting-test test))
     (let* ((package (symbol-name (sting-test-package test)))
            (name (symbol-name (sting-test-name test)))
            (button-size (+ (length package)
                            (length name)
                            3))) ; grab the indicator, the space and the /
-      (insert (propertize package 'face 'font-lock-builtin-face))
-      (insert "/")
-      (insert (propertize name 'face 'font-lock-function-name-face))
+      (insert (propertize package 'face 'font-lock-builtin-face 'sting-test test))
+      (insert (propertize "/" 'sting-test test))
+      (insert (propertize name 'face 'font-lock-function-name-face 'sting-test test))
       (let ((button (make-button (- (point) button-size) (point)
                                  'face nil
                                  'follow-link t
@@ -106,13 +107,21 @@
 
 (repaint-buffer)
 
-(defun sting-expand ()
+(defun sting-run ()
   (interactive)
-  (message "hello"))
+  ;; Rewind one character back until a header is found (it has the needed property sting-test).
+  ;; That way, we can C-c C-c inside the description of an expanded test and still having it run again.
+  (let ((test (loop for p = (point) then (1- p)
+                    for test = (get-text-property p 'sting-test)
+                    while (and (> p 0)
+                               (not test))
+                    finally (return test))))
+    (if test
+        (message "executing %s" test)
+      (message "no test found at point"))))
 
 (with-current-buffer (sting-buffer)
-  (local-set-key (kbd "C-c RET") 'sting-expand)
-  (local-set-key (kbd "C-c C-c") nil))
+  (local-set-key (kbd "C-c C-c") 'sting-run))
 
 ;; At the very end
 (with-current-buffer (sting-buffer)
